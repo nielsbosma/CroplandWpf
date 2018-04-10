e930cbf46a56f5ac92c45416456f8caf85d524c7
Sasha Bodrukhin <dot.het@gmail.com>
2018-04-10 14:44:49 +0300

    - scrollviewer/scrollbar styles - listbox style (still needs some item hover effect)

--- CroplandWpf.Test/MainWindow.xaml.cs
+++ CroplandWpf.Test/MainWindow.xaml.cs
@@ -20,9 +20,18 @@ namespace CroplandWpf.Test
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+		public List<string> TestItemsSource
+		{
+			get { return (List<string>)GetValue(TestItemsSourceProperty); }
+			set { SetValue(TestItemsSourceProperty, value); }
+		}
+		public static readonly DependencyProperty TestItemsSourceProperty =
+			DependencyProperty.Register("TestItemsSource", typeof(List<string>), typeof(MainWindow), new PropertyMetadata());
+
+		public MainWindow()
         {
             InitializeComponent();
+			TestItemsSource = new List<string> { "Item 1", "Item 2", "Some item with looong looooooong text 3", "Item 4", "Item 5", "Item 6", "Item 7", "Another list box item with even more loooooooooooooooooooong text 8", "Item 9", "Item 10", "Item 11", "Item 12" };
         }
     }
 }
