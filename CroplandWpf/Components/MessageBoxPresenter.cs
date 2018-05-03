62049850a77dfbe9774a8bc217ac07aa05922790
Sasha Bodrukhin <dot.het@gmail.com>
2018-05-03 13:42:36 +0300

    - CommandTextBox light buttons mouseover effect

---
+++ CroplandWpf/Components/MessageBoxPresenter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CroplandWpf.Components
+{
+	public class MessageBoxPresenter : Control
+	{
+		public object Content
+		{
+			get { return (object)GetValue(ContentProperty); }
+			set { SetValue(ContentProperty, value); }
+		}
+		public static readonly DependencyProperty ContentProperty =
+			DependencyProperty.Register("Content", typeof(object), typeof(MessageBoxPresenter), new PropertyMetadata());
+
+		static MessageBoxPresenter()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(MessageBoxPresenter), new FrameworkPropertyMetadata(typeof(MessageBoxPresenter)));
+		}
+
+		public MessageBoxPresenter()
+		{
+
+		}
+
+
+	}
+}
