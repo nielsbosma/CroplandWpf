c5cd7f4a8cd6b36c225a82ae85e5295d8cba7465
Sasha Bodrukhin <dot.het@gmail.com>
2018-05-31 21:28:16 +0300

    - BusyIndicator with ripple animation (becomes visible when IsEnabled is set to true - this is the main meaningful property for it)

---
+++ CroplandWpf/Components/BusyIndicator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CroplandWpf.Components
+{
+	public class BusyIndicator : Control
+	{
+		static BusyIndicator()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(BusyIndicator), new FrameworkPropertyMetadata(typeof(BusyIndicator)));
+		}
+
+		public BusyIndicator()
+		{
+			IsEnabled = false;
+			IsHitTestVisible = false;
+		}
+	}
+}
